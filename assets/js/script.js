b3aa4d91ca57767b552184bcf2fb1c05d6f4f9e9
Cyril Leblanc <cyril.leblanc@outlook.com>
2024-09-21 18:38:43 +0200

    cursor animation adjust

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,13 @@ const observer = new IntersectionObserver((entries) => {
 const title = observer.observe(document.querySelector(".projects__title"));
 const projects = document.querySelectorAll(".grid__project");
 projects.forEach((el) => observer.observe(el));
+
+const element = document.querySelector(".type-animation h1:nth-child(2)");
+
+element.addEventListener("animationend", (event) => {
+  if (event.animationName === "cursor") {
+    setTimeout(() => {
+      element.classList.add("cursor-end");
+    }, 500);
+  }
+});
